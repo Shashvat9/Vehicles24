32151602c9cc2fed285b8aa48f4823160407c2b8
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2025-03-18 16:26:50 +0530

    Enhance server setup script to include user input for project cloning and change ownership of web directory

--- deployment/server_setup.sh
+++ deployment/server_setup.sh
@@ -2,16 +2,20 @@
 
 sudo apt update
 
-# now install server for PHP
-
+# Install server for PHP
 sudo apt install apache2
-
 sudo systemctl status apache2
-
 sudo apt install php libapache2-mod-php
-
 sudo systemctl restart apache2
 
 cd /var/www/html
 
-git clone git@github.com:VineetS46/Vehicles24.git
+# Change ownership of /var/www/html
+sudo chown -R ubuntu:ubuntu /var/www/html
+
+# now get the user name and project name from user
+echo "Please enter the user name:"
+read USER_NAME
+echo "Please enter the project name:"
+read PROJECT_NAME
+sudo git clone "https://github.com/$USER_NAME/$PROJECT_NAME.git"
